ae75f458c1f1befb313aa5f8e72f532c3879b55e
Melops <95263124+Melonps@users.noreply.github.com>
2023-01-17 01:23:50 +0900

    [ADD] 本番環境での画像が貼れないので調査
    普通にimportしてみた

--- src/views/Survey.js
+++ src/views/Survey.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import logo from '../images/cpu.png';
 
-
-const Login = () => {
+const Survey = () => {
 
 
     return (
-    <>
-        <h1>ログインページ</h1>
+        <>
+        <h1>実験ページ</h1>
         <img src={logo} className="App-logo" alt="logo" />
         </>
     );
@@ -15,4 +14,4 @@ const Login = () => {
 
 
 
-export default Login;
+export default Survey;
