2ebe592f926417d99d1ab00870730fc7d0a5430e
Melops <95263124+Melonps@users.noreply.github.com>
2023-02-22 17:06:07 +0900

    [FIX] コミット忘れ #3

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
 import 'firebase/app-check'
 
@@ -12,8 +13,14 @@ const firebaseConfig = {
     appId: process.env.REACT_APP_API_APPID,
 };
 
-const app = initializeApp(firebaseConfig).firebase.appCheck().activate(process.env.REACT_APP_PUBLIC_KEY);
+const reCAPTCHA_PUBLIC_KEY = process.env.REACT_APP_PUBLIC_KEY
 
+const app = initializeApp(firebaseConfig)//.firebase.appCheck().activate(reCAPTCHA_PUBLIC_KEY);
+
+export const startAppCheck = () => initializeAppCheck(app, {
+    provider: new ReCaptchaV3Provider(reCAPTCHA_PUBLIC_KEY),
+    isTokenAutoRefreshEnabled: true,
+});
 
 export const db = getFirestore(app);
 
